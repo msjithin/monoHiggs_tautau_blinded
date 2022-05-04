ef2c48517d9024a701f39451e781c73bc40ba5da
Jithin Madhusudanan Sreekala <jithin.madhusudanan.sreekala@cern.ch>
2022-05-04 12:42:56 -0500

    corrects executing steps

--- blinded_2017/etau/postanalyzer/execute_all.sh
+++ blinded_2017/etau/postanalyzer/execute_all.sh
@@ -13,6 +13,6 @@ bash hadd_files.sh all
 bash organize.sh
 bash get_shape_rootfile.sh
 #python gather_hist_v3.py
-#python gather_hist_v4.py
+python gather_hist_v4.py
 #bash do_make_plots.sh
 #bash do_make_plots_wsys.sh
