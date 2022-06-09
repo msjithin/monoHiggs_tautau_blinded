2b60ea7aca83786d3e5352ba99eda71ebc808532
Jithin Madhusudanan Sreekala <jithin.madhusudanan.sreekala@cern.ch>
2022-06-09 11:57:28 -0500

    adds new execution workflow in execute_all.sh

--- blinded_2017/etau/postanalyzer/plotting/execute_all.sh
+++ blinded_2017/etau/postanalyzer/plotting/execute_all.sh
@@ -2,17 +2,4 @@ set -e
 
 # sh copyFiles.sh
 # sh hadd_files.sh
-
-
-# cd plotting_script
-# rm sample/*.root
-#rm *.root
-python postAnalyzer.py 
-
-bash hadd_files.sh all
-bash organize.sh
-bash get_shape_rootfile.sh
-python gather_hist_v3.py
-python gather_hist_v4.py
-#bash do_make_plots.sh
-#bash do_make_plots_wsys.sh
+python main.py -idx 32 -ch etau
